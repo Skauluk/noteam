fb787e738afff281ce6378043805da03beb2ca72
Skauluk <49850186+Skauluk@users.noreply.github.com>
2019-04-22 09:22:32 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function start() {
+	$('html,body').animate({
+        scrollTop: $(".start").offset().top - 70},
+        'slow');
+}
+function between() {
+	$('html,body').animate({
+        scrollTop: $(".between").offset().top - 70},
+        'slow');
+}
+function end() {
+	$('html,body').animate({
+        scrollTop: $(".end").offset().top - 70},
+        'slow');
+}
